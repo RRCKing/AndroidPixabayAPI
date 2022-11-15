87ace23e5df5cfc98eeba46e6f08edf2b4139452
KingChan <bassking123456@gmail.com>
2022-11-14 18:33:57 -0600

    first initial

---
+++ app/src/main/java/com/kingchan/pixalapi/MainActivity.java
@@ -0,0 +1,82 @@
+package com.kingchan.pixalapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    // load from API
+    private RequestQueue requestQueue;
+    private List<Item> mList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // for API
+        requestQueue = VolleySingleton.getmInstance(this).getRequestQueue();
+
+        mList = new ArrayList<>();
+        fetchData();
+    }
+
+    private void fetchData(){
+        String url = "https://pixabay.com/api/?key=30668705-ad44a7c520b986594722a59fa&q=bitcoin&image_type=photo&pretty=true";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("hits");
+
+                    for(int i = 0; i<jsonArray.length(); i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String imageUrl = jsonObject.getString("webformatURL");
+                        int likes = jsonObject.getInt("likes");
+                        String tags = jsonObject.getString("tags");
+
+                        Item post = new Item(imageUrl, tags, likes);
+                        mList.add(post);
+                    }
+
+                    PostAdapter adapter = new PostAdapter(MainActivity.this, mList);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+}
